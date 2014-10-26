46e3c774001e3f5afe255b8311abfef57487205a
Zarthus <buughost@gmail.com>
2014-10-26 12:47:26 +0100

    Finish contact page

--- app/lang/en/contact.php
+++ app/lang/en/contact.php
@@ -2,6 +2,26 @@
 
 return array(
 
-    'header-text' => 'This page is currently under construction.',
+    'header-text' => 'Get in touch with me',
+    'header-sub' => "Contact me though email or Stack Overflow!",
+
+    'main-head' => 'Before you get in touch',
+    'main-body' => 'Would you like to know more about me, or perhaps you just want to chat? That\'s entirely possible!',
+    'aboutme-body' => 'You are free to contact me for whichever reason - but please do note that I am currently still in school, if you are a recruiter, I will not be to travel long distances,  or start immediately on a fulltime job.',
+    'reqport-body' => 'My portfolio is available on request if you contact me through any of the methods of contact below.',
+
+    'email-head' => 'Contact by email',
+    'email-body' => 'Email is my preferred way of getting in touch, simply send an email to <a href="#" class="js-display-email">please enable JavaScript</a>. ' .
+                    '<br>I usually respond within 24 hours.',
+
+    'so-head' => 'Contact through StackOverflow Careers',
+    'so-body' => 'Contacting me through StackOverflow Careers may be the most efficient way to contact me regarding careers, ' .
+                 'the website allows me to give you more extensive contact details on request it is quite pleasant to work with.' .
+                 '<br><a href="https://careers.stackoverflow.com/zarthus">My profile on careers.stackoverflow.com</a>',
+
+    'feedback-head' => 'Feedback on the site',
+    'feedback-body' => 'If you have feedback on this website, that\'s great! All kinds of constructive feedback are appreciated.<br><br> ' .
+                       'You can send in both suggestions and bug reports or things you expected to work differently on the ' .
+                       '<a href="https://github.com/zarthus/josahrens.me/">Github Repository</a> by making a <a href="https://github.com/zarthus/josahrens.me/issues/new">new issue</a>. If there is a security error, please send me an email instead.',
 
 );
--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -7,31 +7,61 @@
 @stop
 
 @section('header')
-<div id="hero-background" class="hero-background-index">
-            <div class="container">
-                <header class="intro">
-                    <div class="hero-unit">
-                        <h1>
-                            {{ trans('home.header-text') }}
-                        </h1>
-                        <h3>
-                            {{ trans('home.header-text-sub') }}
-                        </h3>
+<div id="resume-header" class="text-center">
+    <h1>
+        {{ trans('contact.header-text') }}
+    </h1>
 
-                        <div class="row">
-                            <div class="col-md-offset-4 col-md-4 text-center">
-                                <button id="hero-button" class="btn btn-primary btn-lg">Portfolio <i class="fa fa-arrow-down"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-            </div>
-        </div>
+    <p>
+        {{ trans('contact.header-sub') }}
+    </p>
+</div>
 @stop
 
 @section('content')
 <div class="container">
-    
+    <div class="row">
+        <div class="col-sm-offset-3 col-sm-6">
+            <h2>{{ trans('contact.main-head') }}</h2>
+            <p>
+                {{ trans('contact.main-body') }}
+            </p>
 
+            <p>
+                {{ trans('contact.aboutme-body') }}
+            </p>
+
+            <p>
+                {{ trans('contact.reqport-body') }}
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-offset-3 col-sm-6">
+            <h2>{{ trans('contact.email-head') }}</h2>
+            <p>
+                {{ trans('contact.email-body') }}
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-offset-3 col-sm-6">
+            <h2>{{ trans('contact.so-head') }}</h2>
+            <p>
+                {{ trans('contact.so-body') }}
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-offset-3 col-sm-6">
+            <h2>{{ trans('contact.feedback-head') }}</h2>
+            <p>
+                {{ trans('contact.feedback-body') }}
+            </p>
+        </div>
+    </div>
 </div>
 @stop
